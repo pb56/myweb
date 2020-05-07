92db90d050445e2ede0530d9b21cf97b6c5f5ca4
pb56 <44302852+pb56@users.noreply.github.com>
2020-05-07 17:35:08 +0300

    feat: add normalize, grid, variables, interface header styles in template folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,84 @@
-const {src, dest, watch} = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const minify = require('gulp-minifier');
-const rename = require('gulp-rename');
+const { src, dest, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const minify = require("gulp-minifier");
+const rename = require("gulp-rename");
 
 // Start browser-sync
 function bs() {
-    serveSass(); 
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    watch('./*.html').on('change', browserSync.reload);
-    watch('./sass/**/*.sass').on('change', serveSass);
-    watch('./js/*.js').on('change', browserSync.reload);
-};
+  serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  watch("./*.html").on("change", browserSync.reload);
+  watch("./src/sass/**/*.sass", serveSass);
+  watch("./js/*.js").on("change", browserSync.reload);
+}
 
 // Compiler sass to css
 function serveSass() {
-    return src('./sass/*.sass')
-        .pipe(sass())
-        .pipe(dest('./css'))
-        .pipe(browserSync.stream());
-};
+  return src("./sass/**/*.sass")
+    .pipe(sass())
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
+}
 
-// Mininizer HTML   
+// Mininizer HTML
 function minhtml() {
-    return src('./*.html').pipe(minify({
+  return src("./*.html")
+    .pipe(
+      minify({
         minify: true,
         minifyHTML: {
-        collapseWhitespace: true,
-        conservativeCollapse: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
         },
         getKeptComment: function (content, filePath) {
-            let m = content.match(/\/\*![\s\S]*?\*\//img);
-            return m && m.join('\n') + '\n' || '';
-        }
-    })).pipe(rename({suffix: '.min'})).pipe(dest('./src'));
-};
+          let m = content.match(/\/\*![\s\S]*?\*\//gim);
+          return (m && m.join("\n") + "\n") || "";
+        },
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("./src"));
+}
 
 // Mininizer CSS
 function mincss() {
-    return src('./*.css')
-    .pipe(minify({
+  return src("./*.css")
+    .pipe(
+      minify({
         minify: true,
         minifyCSS: true,
         getKeptComment: function (content, filePath) {
-            let m = content.match(/\/\*![\s\S]*?\*\//img);
-            return m && m.join('\n') + '\n' || '';
-        }
-    })).pipe(rename({suffix: '.min'})).pipe(dest('./src'));
-};
+          let m = content.match(/\/\*![\s\S]*?\*\//gim);
+          return (m && m.join("\n") + "\n") || "";
+        },
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("./src"));
+}
 
 // Mininizer JS
 function minjs() {
-    return src('./*.js').pipe(minify({
+  return src("./*.js")
+    .pipe(
+      minify({
         minify: true,
         minifyJS: {
-        sourceMap: true
+          sourceMap: true,
         },
         getKeptComment: function (content, filePath) {
-            let m = content.match(/\/\*![\s\S]*?\*\//img);
-            return m && m.join('\n') + '\n' || '';
-        }
-    })).pipe(rename({suffix: '.min'})).pipe(dest('./src'));
-};
+          let m = content.match(/\/\*![\s\S]*?\*\//gim);
+          return (m && m.join("\n") + "\n") || "";
+        },
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("./src"));
+}
 
 exports.serve = bs;
