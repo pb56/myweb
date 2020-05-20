44b4fc5947ae265650f9c3636bbf660b1c7fe8f2
pb56 <44302852+pb56@users.noreply.github.com>
2020-05-21 00:00:30 +0300

    feat: add jQuery file and button scroll to up

--- js/main.js
+++ js/main.js
@@ -1,31 +1,55 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+// document.addEventListener("DOMContentLoaded", function(event) { 
  
- const modal = document.querySelector('.modal');
- const modalBtn = document.querySelectorAll('[data-toggle=modal]');
- const closeBtn = document.querySelector('.modal__close');
- const modDial = document.querySelector('modal__dialog');
+//  const modal = document.querySelector('.modal');
+//  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//  const closeBtn = document.querySelector('.modal__close');
+//  const modDial = document.querySelector('modal__dialog');
 
- 
- const switchModal = () => {
-  modal.classList.toggle('modal--visible');
- }
+//  const switchModal = () => {
+//   modal.classList.toggle('modal--visible');
+//  }
 
- modalBtn.forEach(element => {
-  element.addEventListener('click', switchModal);
- });
+//  modalBtn.forEach(element => {
+//   element.addEventListener('click', switchModal);
+//  });
 
- closeBtn.addEventListener('click', switchModal);
+//  closeBtn.addEventListener('click', switchModal);
 
-  modal.addEventListener('click', function (event) {
-    let target = event.target;
-    if (target.classList.className != 'modal__dialog' && target.classList.contains('modal--visible')) {
-     switchModal();
-    }
+//   modal.addEventListener('click', function (event) {
+//     let target = event.target;
+//     if (target.classList.className != 'modal__dialog' && target.classList.contains('modal--visible')) {
+//      switchModal();
+//     }
+//   });
+
+//   document.addEventListener('keydown', function(event) {
+//     if (modal.classList.contains('modal--visible') && event.code == 'Escape') {
+//     switchModal();
+//  }});
+
+// });
+
+$(document).ready(function() {
+  var modal = $('.modal'),
+   modalBtn = $('[data-toggle=modal]'),
+   closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function(){
+   modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function() {
+   modal.toggleClass('modal--visible');
   });
 
-  document.addEventListener('keydown', function(event) {
-    if (modal.classList.contains('modal--visible') && event.code == 'Escape') {
-    switchModal();
- }});
+  $(document).click(function (event) {
+   if ($(event.target).is(modal)) {
+       modal.toggleClass('modal--visible');
+   }
+});
 
+  $(document).keydown(function(event) {
+   if (modal.hasClass('modal--visible') && event.code == 'Escape') {
+       modal.toggleClass('modal--visible');
+   }
+  });
 });
