739f8ad00bfc054864e70b11a9e0fc30c222394c
pb56 <44302852+pb56@users.noreply.github.com>
2020-05-20 17:49:07 +0300

    feat: add function close keybutton ESC, and outmodal

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
  
  const modal = document.querySelector('.modal');
  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
- const closeBtn = document.querySelector('.modal__close'); 
+ const closeBtn = document.querySelector('.modal__close');
+ const modDial = document.querySelector('modal__dialog');
+
+ 
  const switchModal = () => {
-  modal.classList.toggle('modal--visible'); 
+  modal.classList.toggle('modal--visible');
  }
 
  modalBtn.forEach(element => {
@@ -12,5 +15,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
  });
 
  closeBtn.addEventListener('click', switchModal);
+
+  modal.addEventListener('click', function (event) {
+    let target = event.target;
+    if (target.classList.className != 'modal__dialog' && target.classList.contains('modal--visible')) {
+     switchModal();
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (modal.classList.contains('modal--visible') && event.code == 'Escape') {
+    switchModal();
+ }});
 
 });
